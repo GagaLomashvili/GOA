83edfc943a8e06ec9dfea36c379e324923a59291
GagaLomashvili <138609160+GagaLomashvili@users.noreply.github.com>
2023-07-17 11:07:51 -0600

    Add files via upload

---
+++ Day 2/Homework2.py
@@ -0,0 +1,5 @@
+name = "გაბრიელი/Gabriel"
+surname = "ლომაშვილი/Lomashvili"
+age = 13
+height = 1.75
+print(name + " " + surname + " " + str(age) + " " + str(height))
---
+++ Day 3/homework4.py
@@ -0,0 +1,74 @@
+name = Gabriel #true
+surname = Lomashvili
+13 age  correct#
+knows_english="true #true
+favorite_artist=tame_impala'
+favorite_actor= ''Pit Davidson
+favorite_sport= -> soccer #true
+iq->110 #false or true
+worksouts = not to much #I started working on myself since I joined in GOA.
+has_girlfriend = no # maybe it's to distracting for my age right now.
+is_chad =-> true   #only because I joined GOA.
+can_swim-true 
+has_a_pat=true # I have a parrot and a cat.
+favorite_movie= 'Life Is Beautiful
+director- Roberto Benigni
+watches-low-budget-stories= true-
+watches-zero-budget-stories= true-
+favourite_food = -Lobiani"
+
+
+
+
+
+
+has_brother = False
+has_sister  = False 
+has_a_car = True 
+sleeps_early = True 
+plays_games = False #not anymore because im a chad and games are waste of time. (its really hard)
+favorite_subject = Science
+favorite_car = bmwm3 #fornow 
+lives_in = Canada #true 
+has_a_good_pc = True
+likes_swimming = True
+helps_parents = True
+favorite_rapper = Icecube 
+weight = 65
+height = 1.75
+subscribes_gamers = False # I subscribe few of them, and they are my friends.
+loves_coding = True
+loves_singing = True 
+loves_dancing = False
+wants_to_be_pro_coder = True 
+builds_future = True 
+goes_to_school = True # I wanna quit but I cant 
+dream_country = Germany 
+wants_to_meet = TameImpala 
+has_a_usb = True
+born_in = 2010
+birthday_date = 2307
+
+
+
+
+
+
+
+
+
+#correct
+name = "Gabriel"
+surname = "Lomashvili"
+age = 12
+lives_in = "Canada"
+favorite_movie = "Life is beautiful" 
+iq = 110
+favorite_sport = "soccer"
+knows_english = True 
+Joined_in_goa = 20230710
+favorite_food = Lobiani 
+lobiani_is_made_by = Lobio
+has_a_parrot = True
+has_a_cat = True
+is_from = Georgia 
